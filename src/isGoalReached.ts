8d2852b56ae3c7b214d37f6c2b24e9bcbaec991c
shrjrd <153493355+shrjrd@users.noreply.github.com>
2026-01-07 10:37:50 -0600

    replace Object usage

--- src/isGoalReached.ts
+++ src/isGoalReached.ts
@@ -1,11 +1,12 @@
 import type { GoalFacts } from './planner.js'
 
-
-export const isGoalReached = (
-  goal: GoalFacts,
-  state: GoalFacts,
-) => Object
-  .entries(goal)
-  .every(([key, value]) => (typeof value === 'number'
-    ? state[key] as number >= value
-    : state[key] as boolean == value))
+export const isGoalReached = (goal: GoalFacts, state: GoalFacts) => {
+  for (const [key, value] of pairs(goal)) {
+    if (typeIs(value, 'number')) {
+      if (state[key] as number < value) return false
+    } else {
+      if (state[key] as boolean !== value) return false
+    }
+  }
+  return true
+}
